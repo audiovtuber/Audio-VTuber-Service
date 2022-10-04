2266bbf76b608959783f1cefa2acdf9373013757
Alex Service <alex@predictwith.us>
2022-10-03 23:02:46 -0400

    Demo now has a flag button that works, but still needs to save flagged in/out to a cloud resource. Now using a predict function that's about twice as fast as the old one

--- app.py
+++ app.py
@@ -5,10 +5,6 @@ import gradio as gr
 from model import TalkingFaceTorchScript
 
 
-def greet(name):
-    return "Hello " + name + "!"
-
-
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument(
@@ -19,19 +15,55 @@ def parse_args():
     return parser.parse_args()
 
 
+def build_streaming_interface(model):
+    demo = gr.Interface(
+        fn=model.predict_stream,
+        inputs=[gr.Microphone(streaming=True), "state"],
+        outputs=["audio", "plot", "state"],
+        live=True,
+    )
+    return demo
+
+
+def build_static_block(predict_fn):
+
+    # TODO: upload flagged logs (and their blobs) somewhere
+    flag_callback = gr.CSVLogger()
+    with gr.Blocks() as demo:
+        gr.Markdown("Record some audio and see the predicted face shape")
+        with gr.Row():
+            inp = gr.Microphone(label="audio_path", show_label=False)
+            with gr.Column():
+                # NOTE: for whatever reason, regular videos get mirrored by gradio once we add a second button for flagging. Hacky fix: pass `mirror_webcam=False`
+                out = gr.Video(
+                    label="video_path", show_label=False, mirror_webcam=False
+                )
+                flag_btn = gr.Button("Flag", visible=False)
+        btn = gr.Button("Run")
+
+        def predict(args):
+            print(f"args: {args}")
+            return {flag_btn: gr.update(visible=True), out: predict_fn(*args)}
+
+        btn.click(fn=lambda *args: predict(args), inputs=inp, outputs=[flag_btn, out])
+        flag_callback.setup([inp, out], "flagged_data_points")
+        flag_btn.click(
+            lambda *args: flag_callback.flag(args),
+            inputs=[inp, out],
+            outputs=None,
+            preprocess=False,
+        )
+
+    return demo
+
+
 def main(args):
     # TODO: refactor model loading to pull from wandb or other location(s)
     model = TalkingFaceTorchScript(model_path="./torchscript_model.pt")
 
-    with gr.Blocks() as demo:
-        gr.Markdown("Record some audio and see the predicted face shape")
-        with gr.Row():
-            inp = gr.Microphone()
-            out = gr.Video()
-        btn = gr.Button("Run")
-        btn.click(fn=model.predict, inputs=inp, outputs=out)
+    # demo = build_static_block(model.predict)
+    demo = build_static_block(model.predict_animation)
 
-    # demo = gr.Interface(fn=model.predict, inputs=gr.Microphone(), outputs="text")
     # server_name is a workaround to gradio not always exposing the port when working locally in a docker container (e.g. when using WSL2)
     server_name = "0.0.0.0" if args.listen_all else None
     demo.launch(server_name=server_name)
--- model.py
+++ model.py
@@ -1,6 +1,11 @@
+import subprocess
+
 import numpy as np
 import torch
 import librosa
+import matplotlib.animation as animation
+from matplotlib.figure import Figure
+import soundfile as sf
 
 from audio import extract_audio_features, write_video_wpts_wsound
 
@@ -11,7 +16,7 @@ class TalkingFaceTorchScript:
         self.target_sample_rate = target_sample_rate
 
     @torch.no_grad()
-    def predict(self, gradio_audio):
+    def _predict(self, gradio_audio):
         source_sample_rate, source_audio_data = gradio_audio
         print(
             f"Model received audio: {source_audio_data}\nIt has shape {source_audio_data.shape} and type {source_audio_data.dtype}"
@@ -35,9 +40,15 @@ class TalkingFaceTorchScript:
         predictions = self.model.predict(torch.tensor(input_audio))[0]
         predictions = predictions.reshape((-1, 68, 2)).numpy()
 
+        return predictions
+
+    def predict(self, gradio_audio):
+        source_sample_rate, source_audio_data = gradio_audio
+        predictions = self._predict(gradio_audio)
+
         print("Saving Video")
         # NOTE: Skipping Face Normalization (which is normally done during training) because I'm lazy. It seems to be OK without it
-        # TODO: saving the video is a very slow operation
+        # TODO: saving the video is a very slow operation. Check out `predict_animation`, which is closer to realtime
         write_video_wpts_wsound(
             predictions,
             source_audio_data,
@@ -47,6 +58,96 @@ class TalkingFaceTorchScript:
             [0, 1],
             [0, 1],
         )
-
         # TODO: save somewhere else
         return "./results/PD_pts_ws.mp4"
+
+    def predict_stream(self, gradio_audio, state: Figure = None):
+        source_sample_rate, source_audio_data = gradio_audio
+        predictions = self._predict(gradio_audio)
+
+        audio_output = (
+            source_sample_rate,
+            source_audio_data,
+        )
+
+        """
+        from gr.Plot documentation:
+            "As output: expects either a {matplotlib.figure.Figure}, a {plotly.graph_objects._figure.Figure}, or a {dict} corresponding to a bokeh plot (json_item format)"
+        """
+
+        """
+        The plotting code may look scary and if it does, I recommend checking out [this writeup](http://web.archive.org/web/20100830233506/http://matplotlib.sourceforge.net/leftwich_tut.txt)
+        and checking out the matplotlib Object Oriented API
+        Summary: If a figure doesn't already exist, we create one and give it a new axes `ax`. Think of this axes as a layer that we can draw on. We then draw on that layer using `ax.scatter`.
+        The next time this method gets called, we simply remove the old scatter plot data (via `ax.collections[0].remove()` and add our new data
+        """
+        if state is None:
+            state = Figure(figsize=(3.00, 3.00))
+            # The 111 specifies 1 row, 1 column on subplot #1
+            ax = state.add_subplot(111)
+            ax.set_title("Predicted Face Landmarks")
+            ax.set_xlabel("time")
+            ax.set_ylabel("volts")
+            ax.set_xlim(0, 1)
+            ax.set_ylim(0, 1)
+            ax.invert_yaxis()  # otherwise, the face will be upside-down
+        else:
+            ax = state.axes[0]
+            # the next line is a reference to the scatterplot data further below
+            ax.collections[0].remove()
+
+        # These properties must be set everytime, OR the code needs refactored to store a reference to the scatter, which could then be `remove()`
+
+        # TODO: Animate the result. For now, simply get the last frame of the batch
+        x = predictions[-1, :, 0]
+        y = predictions[-1, :, 1]
+
+        ax.scatter(x, y, c="#1f77b4", marker=".")
+
+        plot_output = state
+        return audio_output, plot_output, state
+
+    # This function is generally faster than `predict`. During local testing, it processed 15 seconds of audio in 12 seconds, whereas `predict` took 21 seconds
+    def predict_animation(self, gradio_audio):
+        source_sample_rate, source_audio_data = gradio_audio
+        audio_filename = "source_audio.wav"
+        video_filename = "basic_animation.mp4"
+
+        predictions = self._predict(gradio_audio)
+
+        figure = Figure(figsize=(6.00, 6.00))
+        # The 111 specifies 1 row, 1 column on subplot #1
+        ax = figure.add_subplot(111)
+        ax.set_title("Predicted Face Landmarks")
+        ax.set_xlim(0, 1)
+        ax.set_ylim(0, 1)
+        ax.set_axis_off()
+        ax.invert_yaxis()  # otherwise, the face will be upside-down
+        scatter = ax.scatter([], [], c="#1f77b4", marker=".")
+
+        def init():
+            return (scatter,)
+
+        # https://stackoverflow.com/questions/9401658/how-to-animate-a-scatter-plot
+        def animate(i):
+            scatter.set_offsets(predictions[i])
+            return (scatter,)
+
+        anim = animation.FuncAnimation(
+            figure,
+            animate,
+            init_func=init,
+            frames=predictions.shape[0],
+            interval=40,
+            blit=True,
+        )
+
+        FFwriter = animation.FFMpegWriter(fps=25, extra_args=["-vcodec", "libx264"])
+        anim.save(video_filename, writer=FFwriter)
+
+        # add audio back in
+        sf.write(audio_filename, source_audio_data, source_sample_rate, "PCM_24")
+        cmd = f"ffmpeg -y -i {video_filename} -i {audio_filename} -map 0:v -map 1:a -c:v copy -shortest output.mp4"
+        subprocess.call(cmd, shell=True)
+
+        return "output.mp4"
