f09f88c87fc8371d30fe0e747acfa7664eb01dd7
Alex Service <alex@predictwith.us>
2022-10-14 00:22:51 -0400

    Added comments to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,7 @@ docker tag slashfury/audio-vtuber-service gcr.io/audio-vtuber/audio-vtuber-servi
 # If you don't have gcloud, run `configure_gcp.sh` first
 docker push gcr.io/audio-vtuber/audio-vtuber-service
 gcloud run deploy audio-vtuber-service --image gcr.io/audio-vtuber/audio-vtuber-service:latest
+
+# Tweak machine allocation with the following command(s)
+# gcloud run services update audio-vtuber-service --memory 4Gi --cpu 2
+# gcloud run services update audio-vtuber-service --concurrency 4 --max-instances 10
