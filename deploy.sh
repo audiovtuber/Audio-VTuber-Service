7d7610f6016952fe87dac262200a5d2669fd1edc
Alex Service <alex@predictwith.us>
2022-10-23 20:38:30 -0400

    Renamed docker tags

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Retag docker image ()
-docker tag slashfury/audio-vtuber-service gcr.io/audio-vtuber/audio-vtuber-service
+docker tag aservice/audio-vtuber-service gcr.io/audio-vtuber/audio-vtuber-service
 
 # Push image to GCP Artifact Registry. Requires gcloud installed in your environment
 # If you don't have gcloud, run `configure_gcp.sh` first
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker rm -f av-service
-docker build -t slashfury/audio-vtuber-service .
-docker run --rm -it --name av-service --env PORT=7777 -p 7777:7777 slashfury/audio-vtuber-service
+docker build -t aservice/audio-vtuber-service .
+docker run --rm -it --name av-service --env PORT=7777 -p 7777:7777 aservice/audio-vtuber-service
